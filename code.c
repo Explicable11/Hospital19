42eb69fb65c3d7c3e1bea64880b36a5d0fc824f5
Dev Ayush <143614582+Explicable11@users.noreply.github.com>
2025-07-01 07:12:37 +0530

    Update code.c

--- code.c
+++ code.c
@@ -46,7 +46,7 @@ public class HospitalManagementSystem extends JFrame {
 
         headerPanel.add(headerLabel);
 
-        // button panel
+        // Button panel
         JPanel buttonPanel = new JPanel(new GridBagLayout());
         buttonPanel.setOpaque(false); // Make button panel transparent
         buttonPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
